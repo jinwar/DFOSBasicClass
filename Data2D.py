a8be37a06da1d5421e7e0848456eac555c345c54
Ge Jin <jinwar@gmail.com>
2020-12-25 11:07:36 -0600

    Update Data2D.py

--- Data2D.py
+++ Data2D.py
@@ -11,40 +11,46 @@ class Data2D():
         self.data = None   # data, 2D array
         self.start_time = None  # starting time using datetime
         self.taxis = []  # time axis in second from start_time
-        self.chan = [] # fiber distance
-        self.x = []  # fiber physical location
-        self.y = []  # fiber physical location
-        self.z = []  # fiber physical location
-        self.md = []  # fiber measured depth along the well
-        self.attrs = {'Python Class Version':'1.0'} # data attributes
+        self.chan = [] # fiber channel number
+        self.md = []  # fiber physical distance or location
+        self.attrs = {'Python Class Version':'1.1'} # data attributes
     
     def saveh5(self,filename):
         f = h5py.File(filename,'w')
+        # save main dataset
         dset = f.create_dataset('data',data=self.data)
-        f.create_dataset('chan',data=self.chan)
-        f.create_dataset('taxis',data=self.taxis)
-        f.create_dataset('x',data=self.x)
-        f.create_dataset('y',data=self.y)
-        f.create_dataset('z',data=self.z)
-        f.create_dataset('md',data=self.md)
+        # save all the attributes to the main dataset
         dset.attrs['start time'] = self.start_time.strftime('%Y%m%d_%H%M%S.%f')
         for k in self.attrs.keys():
             dset.attrs[k] = self.attrs[k]
+        # save all other ndarray and lists in the class
+        for k in self.__dict__.keys():
+            if k == 'data':
+                continue
+            if k == 'start_time':
+                continue
+            if k == 'attrs':
+                continue
+            dtype = type(self.__dict__[k])
+            if dtype == np.ndarray:
+                f.create_dataset(k,data=self.__dict__[k])
+            if dtype == list:
+                f.create_dataset(k,data=self.__dict__[k])
+
         f.close()
 
 
     def loadh5(self,filename):
         f = h5py.File(filename,'r')
-        self.data = f['data'][:,:]
-        self.taxis = f['taxis'][:]
-        self.chan = f['chan'][:]
-        self.x = f['x'][:]
-        self.y = f['y'][:]
-        self.z = f['z'][:]
+        # read start_time
         self.start_time = datetime.strptime(f['data'].attrs['start time'],'%Y%m%d_%H%M%S.%f')
+        # read attributes
         self.attrs = {}
         for k in f['data'].attrs.keys():
             if k == 'start time':
                 continue
             self.attrs[k] = f['data'].attrs[k]
+        # read all other ndarrays
+        for k in f.keys():
+            setattr(self,k,np.array(f[k]))
         f.close()
